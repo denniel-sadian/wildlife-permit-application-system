65cf7f255bd4291fb69da3fe647898ac04c0930d
Denniel <sadiandenniel@gmail.com>
2023-09-05 16:09:42 +0800

    Update fields of Permittee model

--- users/models.py
+++ users/models.py
@@ -93,7 +93,12 @@ class Permittee(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     address = models.TextField()
-    farm_name = models.CharField(max_length=255)
-    farm_address = models.TextField()
-    permittee_photo = models.ImageField(upload_to='uploaded-media/')
-    farm_photo = models.ImageField(upload_to='uploaded-media/')
+    farm_name = models.CharField(max_length=255, blank=True, null=True)
+    farm_address = models.TextField(blank=True, null=True)
+    permittee_photo = models.ImageField(
+        upload_to='uploaded-media/', blank=True, null=True)
+    farm_photo = models.ImageField(
+        upload_to='uploaded-media/', blank=True, null=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
