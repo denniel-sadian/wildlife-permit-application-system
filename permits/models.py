7c3ee4141d3f37995e129970c6fd9da009645b28
Denniel <sadiandenniel@gmail.com>
2023-09-09 22:15:16 +0800

    Edit models

--- permits/models.py
+++ permits/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from model_utils.managers import InheritanceManager
 
 from animals.models import SubSpecies
+from users.models import Client, Permittee
 
 
 class Status(models.TextChoices):
@@ -20,13 +21,27 @@ class PermitType(models.TextChoices):
     LTP = 'LTP', 'Local Transport Permit'
 
 
+class RequirementType(models.TextChoices):
+    REQUIREMENT_1 = 'REQUIREMENT_1', ' Requirement 1'
+    REQUIREMENT_2 = 'REQUIREMENT_2', ' Requirement 2'
+
+
 class Permit(models.Model):
     permit_no = models.CharField(max_length=255)
     status = models.CharField(choices=Status.choices, max_length=50)
+    client = models.ForeignKey(
+        Client, on_delete=models.CASCADE, related_name='permits')
+    permittee = models.ForeignKey(
+        Permittee, on_delete=models.CASCADE, blank=True, null=True, related_name='temporarily_assigned_permits')
+    uploaded_file = models.FileField(
+        upload_to='uploads/', null=True, blank=True)
     valid_until = models.DateField(null=True, blank=True)
     created_at = models.DateField(auto_now_add=True)
 
     objects = InheritanceManager()
+
+    def __str__(self):
+        return str(self.permit_no)
 
 
 class WildlifeFarmPermit(Permit):
@@ -45,8 +60,11 @@ class PermittedToCollectAnimal(models.Model):
     sub_species = models.ForeignKey(
         SubSpecies, on_delete=models.CASCADE, related_name='species_permitted')
     wcp = models.ForeignKey(
-        SubSpecies, on_delete=models.CASCADE, related_name='wcp_species')
+        WildlifeCollectorPermit, on_delete=models.CASCADE, related_name='wcp_species')
     quantity = models.IntegerField()
+
+    class Meta:
+        unique_together = ('sub_species', 'wcp')
 
 
 class LocalTransportPermit(Permit):
@@ -67,3 +85,20 @@ class TransportEntry(models.Model):
     ltp = models.ForeignKey(
         LocalTransportPermit, on_delete=models.CASCADE, related_name='species_to_transport')
     quantity = models.IntegerField()
+
+
+class PermitApplication(models.Model):
+    client = models.ForeignKey(
+        Client, on_delete=models.CASCADE, related_name='permit_applications')
+    permit_type = models.CharField(max_length=50, choices=PermitType.choices)
+    status = models.CharField(choices=Status.choices, max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Requirement(models.Model):
+    permit_application = models.ForeignKey(
+        PermitApplication, on_delete=models.CASCADE)
+    requirement_type = models.CharField(
+        max_length=50, choices=RequirementType.choices)
+    uploaded_file = models.FileField(upload_to='uploads/')
