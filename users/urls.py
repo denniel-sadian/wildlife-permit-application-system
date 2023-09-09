ae70ad34510a2218ffd23157ee714e163bf8cb26
Denniel <sadiandenniel@gmail.com>
2023-09-09 22:14:04 +0800

    Host media files

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
+    path('', views.HomeView.as_view(), name='home'),
+    path('auth/', include('django.contrib.auth.urls')),
     path('registration/', views.ClientRegistrationView.as_view(),
-         name='client-registration')
+         name='registration')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
