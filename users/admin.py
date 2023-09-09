7e37ecff5290c99a6e2049122e7e0f986baf8fcd
Denniel <sadiandenniel@gmail.com>
2023-09-09 23:06:30 +0800

    Add the date_joined

--- users/admin.py
+++ users/admin.py
@@ -12,7 +12,7 @@ class BaseUserAdmin(UserAdmin):
         }),
     )
     list_display = ('username', 'email', 'first_name',
-                    'last_name', 'is_active', 'last_login')
+                    'last_name', 'is_active', 'date_joined', 'last_login')
     fieldsets = (
         ('Authentication', {'fields': ('username', 'password')}),
         ('Personal Information', {
