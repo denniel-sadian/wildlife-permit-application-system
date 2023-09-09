4a38abe7ceb3093d233ab2d50007454772f2dd3f
Denniel <sadiandenniel@gmail.com>
2023-09-09 22:15:38 +0800

    Add admin for permits

--- permits/admin.py
+++ permits/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import WildlifeFarmPermit, WildlifeCollectorPermit, PermittedToCollectAnimal
+
+
+@admin.register(WildlifeFarmPermit)
+class WildlifeFarmPermitAdmin(admin.ModelAdmin):
+    list_display = ('permit_no', 'status', 'client')
+
+
+class PermittedToCollectAnimalInline(admin.TabularInline):
+    model = PermittedToCollectAnimal
+    extra = 1
+
+
+@admin.register(WildlifeCollectorPermit)
+class WildlifeCollectorPermitAdmin(admin.ModelAdmin):
+    list_display = ('permit_no', 'status', 'client')
+    inlines = (PermittedToCollectAnimalInline,)
