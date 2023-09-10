3ce19be8d11c111cd6586c4edf19d0e2cc4e5a2b
Denniel <sadiandenniel@gmail.com>
2023-09-10 18:31:27 +0800

    Assign the present permits

--- users/signals.py
+++ users/signals.py
@@ -21,13 +21,17 @@ def post_save_admin(sender, instance: Admin, created, **kwargs):
 
 
 @receiver(user_logged_in)
-def client_first_login(sender, user, request, **kwargs):
+def client_first_login(sender, user: User, request, **kwargs):
     """Clients' login signal."""
     if user.type is Client.__name__ and not user.is_initial_password_changed:
-        # TODO: Get all attached concerns in the temporary permittee record and attach them to the new client
         found_temp_permittee = Permittee.objects.filter(
-            first_name__icontains=user.first_name, last_name__icontains=user.last_name)
+            first_name__icontains=user.first_name, last_name__icontains=user.last_name).first()
+        if found_temp_permittee:
 
-        # Make sure the temporary permitee is now deleted because the client registered already
-        if found_temp_permittee is not None:
+            # Assign the present permits
+            for permit in found_temp_permittee.temporarily_assigned_permits.all():
+                permit.client = user.subclass
+                permit.permittee = None
+                permit.save()
+
             found_temp_permittee.delete()
