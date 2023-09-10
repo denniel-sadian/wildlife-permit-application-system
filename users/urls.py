698f8dba064d5dbdef2cf524a6287bd949591c36
Denniel <sadiandenniel@gmail.com>
2023-09-10 14:08:49 +0800

    Register CustomPasswordChangeView

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,8 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('auth/password_change/', views.CustomPasswordChangeView.as_view(),
+         name='password_change'),
     path('auth/', include('django.contrib.auth.urls')),
     path('registration/', views.ClientRegistrationView.as_view(),
          name='registration')
