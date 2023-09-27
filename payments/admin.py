42516107d25a734347f933955373fdc0d54aa9a5
Denniel <sadiandenniel@gmail.com>
2023-09-27 23:55:41 +0800

    Update `OrderOfPaymentAdmin`

--- payments/admin.py
+++ payments/admin.py
@@ -1,5 +1,7 @@
 from typing import Any
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
 
 from .models import (
     OrderOfPayment,
@@ -19,6 +21,20 @@ class OrderOfPaymentAdmin(admin.ModelAdmin):
               'client', 'permit_application', 'approved_by')
     autocomplete_fields = ('permit_application', 'client')
     inlines = (OItemInline,)
+    change_form_template = 'payments/admin/op_changeform.html'
+
+    def get_readonly_fields(self, request, obj=None):
+        # If obj is None, it means we are adding a new record
+        if obj is None:
+            return ()
+        # Otherwise, when updating an existing record
+        return ('client', 'client', 'permit_application')
+
+    def response_change(self, request, obj: OrderOfPayment):
+        if 'view_application' in request.POST:
+            path = f'admin:{obj.permit_application._meta.app_label}_{obj.permit_application._meta.model_name}_change'
+            return HttpResponseRedirect(reverse_lazy(path, args=[obj.permit_application.id]))
+        return super().response_change(request, obj)
 
     def save_model(self, request: Any, obj: Any, form: Any, change: Any) -> None:
         if not change:
