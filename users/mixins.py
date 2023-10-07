d9cbda8a0c69d91d452736342137df23c82dffe0
Denniel <sadiandenniel@gmail.com>
2023-10-07 21:21:33 +0800

    Update mixins

--- users/mixins.py
+++ users/mixins.py
@@ -1,4 +1,5 @@
 from django.urls import reverse_lazy
+from django.core.exceptions import ValidationError
 
 
 class ModelMixin:
@@ -21,3 +22,12 @@ class ModelMixin:
     def admin_url(self):
         path = f'admin:{self._meta.app_label}_{self._meta.model_name}_change'
         return reverse_lazy(path, args=[self.id])
+
+
+def validate_file_extension(value):
+    accepted_types = ['pdf', 'jpg', 'jpeg', 'png']
+    for i in accepted_types:
+        if value.name.lower().endswith('.'+i):
+            return
+    raise ValidationError(
+        'File types that are only allowed: ' + (', ').join(accepted_types))
