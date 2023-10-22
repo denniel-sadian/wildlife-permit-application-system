89f1f201992d802841fbecec15bf65febac1103a
Denniel <sadiandenniel@gmail.com>
2023-10-23 00:38:33 +0800

    Create `PermitFilter`

--- permits/filters.py
+++ permits/filters.py
@@ -5,7 +5,14 @@ from users.models import (
 )
 
 from .models import (
-    PermitApplication
+    PermitApplication,
+    Permit,
+    PermitType,
+    LocalTransportPermit,
+    WildlifeFarmPermit,
+    WildlifeCollectorPermit,
+    CertificateOfWildlifeRegistration,
+    GratuitousPermit
 )
 
 
@@ -21,11 +28,49 @@ class PermitApplicationFilter(django_filters.FilterSet):
     def qs(self):
         qs = super().qs
         user = getattr(self.request, 'user', None)
-        if user.type == Client.__name__:
-            return qs.filter(client=user.subclass)
-        return qs
+        return qs.filter(client=user.subclass)
 
     def search_filter(self, queryset, name, value):
         return queryset.filter(**{
             'no__icontains': value,
         })
+
+
+class PermitFilter(django_filters.FilterSet):
+    search = django_filters.CharFilter(
+        label='Search', method='search_filter')
+    permit_type = django_filters.ChoiceFilter(
+        label='Permit Type', choices=PermitType.choices,
+        method='permit_type_filter')
+
+    class Meta:
+        model = Permit
+        fields = ['permit_no', 'status']
+
+    def __init__(self, data=None, queryset=None, *, request=None, prefix=None):
+        super().__init__(data, queryset, request=request, prefix=prefix)
+        subclasses = {
+            PermitType.LTP: LocalTransportPermit,
+            PermitType.WFP: WildlifeFarmPermit,
+            PermitType.WCP: WildlifeCollectorPermit,
+            PermitType.CWR: CertificateOfWildlifeRegistration,
+            PermitType.GP: GratuitousPermit
+        }
+        if data and 'permit_type' in data and data['permit_type']:
+            self.queryset = subclasses[data['permit_type']].objects
+
+    @property
+    def qs(self):
+        qs = super().qs
+        user = getattr(self.request, 'user', None)
+        if isinstance(user.subclass, Client):
+            return qs.filter(client=user.subclass)
+        return qs
+
+    def search_filter(self, queryset, name, value):
+        return queryset.filter(**{
+            'permit_no__icontains': value,
+        })
+
+    def permit_type_filter(self, queryset, name, value):
+        return queryset
