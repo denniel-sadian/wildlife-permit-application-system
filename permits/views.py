82b3898f8a9c2dbceedab23898bce7ffc909ad27
Denniel <sadiandenniel@gmail.com>
2023-10-23 01:04:29 +0800

    Override the querysets

--- permits/views.py
+++ permits/views.py
@@ -17,7 +17,6 @@ from django.utils.http import urlencode
 from users.views import CustomLoginRequiredMixin
 from .models import (
     PermitApplication,
-    PermitType,
     Status,
     UploadedRequirement,
     TransportEntry,
@@ -36,37 +35,6 @@ from .filters import (
     PermitApplicationFilter,
     PermitFilter
 )
-
-
-class QueryParamFilterMixin:
-
-    def get_query_filters(self):
-        original_filters = {}
-        for k, v in self.request.GET.items():
-            if k.split('__')[0] in self.filter_fields:
-                original_filters[k] = v
-
-        modified_filters = {}
-        for k, v in original_filters.items():
-            if v == None or (v != None and v == 'ALL'):
-                continue
-            modified_filters[k] = v
-
-        return original_filters, modified_filters
-
-    def get_queryset(self) -> QuerySet[Any]:
-        filters = self.get_query_filters()[1]
-
-        queryset = self.model.objects.filter(
-            client=self.request.user, **filters).order_by('-created_at')
-        return queryset
-
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        context['permit_types'] = PermitType.choices
-        context['statuses'] = Status.choices
-        context.update(self.get_query_filters()[0])
-        return context
 
 
 class PermitApplicationCreateView(CustomLoginRequiredMixin, CreateView):
@@ -125,6 +93,9 @@ class PermitApplicationUpdateView(CustomLoginRequiredMixin, UpdateView):
     form_class = PermitApplicationUpdateForm
     template_name = 'permits/update_application.html'
     last_edited_list = None
+
+    def get_queryset(self):
+        return super().get_queryset().filter(client=self.request.user)
 
     def get_success_url(self) -> str:
         return reverse_lazy(
@@ -215,12 +186,19 @@ class PermitApplicationDeleteView(DeleteView):
     template_name = 'permits/confirm_delete_permit_application.html'
     success_url = reverse_lazy('list_applications')
 
+    def get_queryset(self):
+        return super().get_queryset().filter(client=self.request.user)
+
 
 class SubmitRedirectView(CustomLoginRequiredMixin, SingleObjectMixin, RedirectView):
     model = PermitApplication
 
+    def get_queryset(self):
+        return super().get_queryset().filter(client=self.request.user)
+
     def get_redirect_url(self, *args: Any, **kwargs: Any):
-        permit_application: PermitApplication = self.get_object()
+        permit_application: PermitApplication = self.get_object(
+            self.get_queryset())
         same_url = reverse_lazy('update_application', kwargs={
                                 'pk': permit_application.id})
 
