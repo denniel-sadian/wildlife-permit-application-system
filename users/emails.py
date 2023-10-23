d1aaa7e95aea0996ee5d39b7e75e021390302d0c
Denniel <sadiandenniel@gmail.com>
2023-10-23 23:10:57 +0800

    Create and use `EmailContextMixin`

--- users/emails.py
+++ users/emails.py
@@ -1,19 +1,35 @@
 from django_yubin.message_views import TemplatedHTMLEmailMessageView
 
+from biodiversity.context_processors import custom_global_vars
 
-class RegistrationEmailView(TemplatedHTMLEmailMessageView):
+
+class EmailContextMixin:
+
+    def __init__(self, user, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user = user
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(custom_global_vars(None))
+
+        if hasattr(self, 'user'):
+            context['user'] = self.user
+
+        return context
+
+
+class RegistrationEmailView(EmailContextMixin, TemplatedHTMLEmailMessageView):
     subject_template_name = 'users/emails/registration/subject.txt'
     body_template_name = 'users/emails/registration/body.txt'
     html_body_template_name = 'users/emails/registration/body.html'
 
     def __init__(self, user, temporary_password, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.user = user
+        super().__init__(user, *args, **kwargs)
         self.temporary_password = temporary_password
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['user'] = self.user
         context['temporary_password'] = self.temporary_password
         return context
 
