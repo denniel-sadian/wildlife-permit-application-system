b47526520910dfaca59202f808b98c9fa3704d8e
Denniel <sadiandenniel@gmail.com>
2023-10-23 22:26:34 +0800

    Register `context_processors`

--- biodiversity/settings.py
+++ biodiversity/settings.py
@@ -87,6 +87,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'biodiversity.context_processors.custom_global_vars',
             ],
         },
     },
