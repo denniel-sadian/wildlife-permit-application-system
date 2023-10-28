17f33d93cc95af15a4fd48ca4c260f83c3460bd9
Denniel <sadiandenniel@gmail.com>
2023-10-29 02:40:05 +0800

    Create signal `application_accepted`

--- permits/signals.py
+++ permits/signals.py
@@ -8,7 +8,8 @@ from .models import (
 )
 from .tasks import (
     notify_admins_about_submitted_application,
-    notify_admins_about_unsubmitted_application
+    notify_admins_about_unsubmitted_application,
+    notify_client_about_accepted_application
 )
 
 
@@ -17,6 +18,7 @@ logger = logging.getLogger(__name__)
 
 application_submitted = Signal()
 application_unsubmitted = Signal()
+application_accepted = Signal()
 
 
 @receiver(application_submitted)
@@ -30,4 +32,11 @@ def receive_application_submitted(sender, application: PermitApplication, **kwar
 def receive_application_unsubmitted(sender, application: PermitApplication, **kwargs):
     logger.info('Permit application %s is unsubmitted.', application.no)
     notify_admins_about_unsubmitted_application.delay(
+        application_id=application.id)
+
+
+@receiver(application_accepted)
+def receive_application_accepted(sender, application: PermitApplication, **kwargs):
+    logger.info('Permit application %s is accepted.', application.no)
+    notify_client_about_accepted_application.delay(
         application_id=application.id)
