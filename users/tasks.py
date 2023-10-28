02e2b193f69ea0c8803b2132a7a26698326ee236
Denniel <sadiandenniel@gmail.com>
2023-10-29 00:13:49 +0800

    Update logging messages

--- users/tasks.py
+++ users/tasks.py
@@ -12,6 +12,8 @@ logger = logging.getLogger(__name__)
 @shared_task
 def send_account_created_email(user_id, temporary_password):
     user = User.objects.get(id=user_id)
-    logger.info('Sending account created email to %s...', user.email)
+    logger.info('Sending account created email to %s %s...',
+                user.type, user.email)
     RegistrationEmailView(user, temporary_password).send()
-    logger.info('Account created email sent to %s.', user.email)
+    logger.info('Account created email sent to %s %s.',
+                user.type, user.email)
