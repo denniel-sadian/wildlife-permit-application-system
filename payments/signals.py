72cc39abf8de335eb97ba114df174828dae745b0
Denniel <sadiandenniel@gmail.com>
2023-10-29 04:09:28 +0800

    Create signal `payment_order_released`

--- payments/signals.py
+++ payments/signals.py
@@ -8,7 +8,8 @@ from .models import (
 )
 from .tasks import (
     notify_signatories_about_prepared_payment_order,
-    notify_admins_about_signed_payment_order
+    notify_admins_about_signed_payment_order,
+    notify_client_about_released_payment_order
 )
 
 
@@ -17,6 +18,7 @@ logger = logging.getLogger(__name__)
 
 payment_order_prepared = Signal()
 payment_order_signed = Signal()
+payment_order_released = Signal()
 
 
 @receiver(payment_order_prepared)
@@ -30,4 +32,11 @@ def receive_application_submitted(sender, payment_order: PaymentOrder, **kwargs)
 def receive_payment_order_signed(sender, payment_order: PaymentOrder, **kwargs):
     logger.info('Payment order %s has been signed.', payment_order.no)
     notify_admins_about_signed_payment_order.delay(
+        payment_order_id=payment_order.id)
+
+
+@receiver(payment_order_released)
+def receive_payment_order_released(sender, payment_order: PaymentOrder, **kwargs):
+    logger.info('Payment order %s has been released.', payment_order.no)
+    notify_client_about_released_payment_order.delay(
         payment_order_id=payment_order.id)
