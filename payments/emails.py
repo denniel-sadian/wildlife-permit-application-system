128d2547450d9ad424cb2134dbdf8f5689e3e84a
Denniel <sadiandenniel@gmail.com>
2023-10-29 03:37:30 +0800

    Create email `PreparedPaymentOrderEmailView`

---
+++ payments/emails.py
@@ -0,0 +1,21 @@
+from django_yubin.message_views import TemplatedHTMLEmailMessageView
+
+from users.emails import EmailContextMixin
+
+
+class BasePaymentOrderEmailView(EmailContextMixin, TemplatedHTMLEmailMessageView):
+
+    def __init__(self, user, payment_order, *args, **kwargs):
+        super().__init__(user, *args, **kwargs)
+        self.payment_order = payment_order
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['payment_order'] = self.payment_order
+        return context
+
+
+class PreparedPaymentOrderEmailView(BasePaymentOrderEmailView):
+    subject_template_name = 'payments/emails/paymentorder_preped/subject.txt'
+    body_template_name = 'payments/emails/paymentorder_preped/body.txt'
+    html_body_template_name = 'payments/emails/paymentorder_preped/body.html'
