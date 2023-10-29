c00c7931a507e2c041fee130926480be9ca5c8e0
Denniel <sadiandenniel@gmail.com>
2023-10-29 13:34:47 +0800

    Create email `PermitReleasedEmailView`

--- permits/emails.py
+++ permits/emails.py
@@ -73,3 +73,9 @@ class PermitCreatedEmailView(BasePermitEmailView):
     subject_template_name = 'permits/emails/permit_created/subject.txt'
     body_template_name = 'permits/emails/permit_created/body.txt'
     html_body_template_name = 'permits/emails/permit_created/body.html'
+
+
+class PermitReleasedEmailView(BasePermitEmailView):
+    subject_template_name = 'permits/emails/permit_released/subject.txt'
+    body_template_name = 'permits/emails/permit_released/body.txt'
+    html_body_template_name = 'permits/emails/permit_released/body.html'
