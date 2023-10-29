031596e7d3b8e3d15b4d590145fd204a0530faa2
Denniel <sadiandenniel@gmail.com>
2023-10-30 01:45:44 +0800

    Register webhook view

--- payments/urls.py
+++ payments/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('payments/pay_via_gcash_redirect/<int:pk>/', views.PayViaGcashRedirectView.as_view(),
          name='pay_via_gcash_redirect'),
     path('payments/authorization_complete/<int:pk>/', views.AuthorizationCompleteDetailView.as_view(),
-         name='authorization_complete')
+         name='authorization_complete'),
+    path('payments/webhook/', views.webhook, name='webhook')
 ]
