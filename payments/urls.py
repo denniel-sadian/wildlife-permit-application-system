796ce23d8bfbbfb4d2a2ca5ff5e02c83f34a98ff
Denniel <sadiandenniel@gmail.com>
2023-10-30 01:36:34 +0800

    Create urls

---
+++ payments/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('payments/pay_via_gcash_redirect/<int:pk>/', views.PayViaGcashRedirectView.as_view(),
+         name='pay_via_gcash_redirect'),
+    path('payments/authorization_complete/<int:pk>/', views.AuthorizationCompleteDetailView.as_view(),
+         name='authorization_complete')
+]
