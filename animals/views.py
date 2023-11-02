e5524af1d532a2dcbd096f9ead1adc4c906aa84d
Denniel <sadiandenniel@gmail.com>
2023-11-02 11:48:48 +0800

    Create view `TransportStatsView`

--- animals/views.py
+++ animals/views.py
@@ -1,22 +1,57 @@
-from dal import autocomplete
+import base64
+import calendar
+import datetime
+import json
+from typing import Any
 
-from .models import (
-    SubSpecies
+from django.views.generic import TemplateView
+from django.db.models import Sum, F
+
+from users.views import CustomLoginRequiredMixin
+from users.models import Client
+
+from permits.models import (
+    TransportEntry,
+    Status
 )
 
+from .models import Species
 
-class SubSpeciesAutocompleteView(autocomplete.Select2QuerySetView):
 
-    def get_queryset(self):
-        # Don't forget to filter out results depending on the visitor !
-        if not self.request.user.is_authenticated:
-            return SubSpecies.objects.none()
+class TransportStatsView(CustomLoginRequiredMixin, TemplateView):
+    template_name = 'animals/transport_stats.html'
 
-        qs = SubSpecies.objects.all()
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['tab'] = 'transport_stats'
 
-        if self.q:
-            qs = qs.filter(common_name__istartswith=self.q,
-                           scientific_name__istartswith=self.q,
-                           main_species__name__istartswith=self.q)
+        selected_year = int(self.request.GET.get(
+            'year', datetime.datetime.now().year))
+        context['year'] = selected_year
+        data = {}
+        for month in range(1, 13):
+            from_date = f'{selected_year}-{month:02d}-01'
+            to_date = f'{selected_year}-{month:02d}-{calendar.monthrange(selected_year, month)[1]}'
 
-        return qs
+            for species in Species.objects.all():
+                if species.name not in data:
+                    data[species.name] = []
+
+                filters = {
+                    'ltp__status__in': [Status.RELEASED, Status.USED],
+                    'ltp__transport_date__gte': from_date,
+                    'ltp__transport_date__lte': to_date,
+                    'sub_species__main_species': species
+                }
+                if isinstance(self.request.user.subclass, Client):
+                    filters['ltp__client'] = self.request.user.subclass
+
+                total = TransportEntry.objects \
+                    .filter(**filters) \
+                    .aggregate(total=Sum(F('quantity')))['total']
+                data[species.name].append(total or 0)
+
+        context['data'] = base64.urlsafe_b64encode(
+            json.dumps(data).encode('utf-8')).decode('utf-8')
+
+        return context
