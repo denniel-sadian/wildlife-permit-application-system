013f61ad64569bd88f413c3184564c15bd774a14
Denniel <sadiandenniel@gmail.com>
2023-11-08 10:34:41 +0800

    Register `GenerateReportsRedirectView` in the urls

--- animals/urls.py
+++ animals/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    # Autocomplete
     path('species/transport_stats/', views.TransportStatsView.as_view(),
          name='transport_stats'),
+    path('species/generate_reports/', views.GenerateReportsRedirectView.as_view(),
+         name='generate_reports'),
 ]
