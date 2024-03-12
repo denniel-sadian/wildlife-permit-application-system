038c26090672d59866ef89819d5551007d517cbf
Denniel <sadiandenniel@gmail.com>
2024-03-12 09:24:30 +0800

    Update frontend

---
+++ frontend/src/components/NotificationItem.vue
@@ -0,0 +1,82 @@
+<script setup>
+import { ref, computed, onMounted } from 'vue';
+
+const props = defineProps({
+    item: Object,
+});
+
+const data = ref({});
+
+const dateDisplay = computed(() => {
+    if (data) {
+        const date = new Date(data.value.created_at);
+        return date.toLocaleString();
+    }
+});
+
+async function markRead() {
+    if (!data.value.read) {
+        await fetch(`/api/notifications/${data.value.id}/`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
+            },
+            body: JSON.stringify({read: true})
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then((newData) => {
+            data.value = newData;
+        });
+    }
+
+    if (data.value.url) {
+        window.location.href = data.value.url;
+    }
+}
+
+onMounted(async () => {
+    data.value = props.item;
+});
+</script>
+
+<template>
+<div class="notif-item" :class="{'unread': !data.read}" @click="markRead">
+    <i class="fa fa-info-circle"></i>
+    <span class="notif-msg">{{ data.message }}</span>
+    <span class="notif-date">{{ dateDisplay }} - {{ data.url }}</span>
+</div>
+</template>
+
+<style scoped>
+.notif-item {
+    display: flex;
+    flex-direction: column;
+    margin: 0px 20px 10px 20px;
+    border-radius: 12px;
+    padding: 20px 10px 6px 10px;
+    transition: 0.3s;
+    background-color: #f3f3f3;
+}
+
+.unread {
+    background-color: #ddffff;
+}
+
+.notif-item:hover {
+    background-color: #87CEEB;
+    cursor: pointer;
+}
+
+.notif-msg {
+    margin-top: 8px;
+}
+
+.notif-date {
+    text-align: right;
+    color: gray;
+    font-size: 10px;
+}
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,15 +8,25 @@ createApp(App).mount('#app')
 */
 
 import 'vite/modulepreload-polyfill'; // required for vite entrypoints
+import { ObserveVisibility } from 'vue-observe-visibility';
 
 import { createApp, defineComponent } from 'vue/dist/vue.esm-bundler.js';
-import HelloWorld from './components/HelloWorld.vue'
+import NotificationList from './components/NotificationList.vue';
 
 const RootComponent = defineComponent({
   components: {
-    'hello-world': HelloWorld,
+    'notification-list': NotificationList,
   },
 });
 
 const app = createApp(RootComponent);
+app.directive('observe-visibility', {
+  beforeMount: (el, binding, vnode) => {
+    vnode.context = binding.instance;
+    ObserveVisibility.bind(el, binding, vnode);
+  },
+  update: ObserveVisibility.update,
+  unmounted: ObserveVisibility.unbind,
+});
+
 app.mount('#app');
