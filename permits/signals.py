f87fe16fcdbe72d0c177c35ca709a25d53326d76
Denniel <sadiandenniel@gmail.com>
2024-03-12 10:15:16 +0800

    Create notifications for known processes

--- permits/signals.py
+++ permits/signals.py
@@ -23,7 +23,9 @@ from .tasks import (
     notify_signatories_about_created_permit,
     notify_admins_about_signed_permit,
     notify_client_and_admins_about_released_permit,
-    notify_client_and_admins_about_validated_permit
+    notify_client_and_admins_about_validated_permit,
+    get_admins_who_can_receive_emails,
+    get_permit_signatories_who_can_receive_emails
 )
 
 
@@ -61,6 +63,22 @@ def receive_application_submitted(sender, application: PermitApplication, **kwar
         )
 
 
+@receiver(application_submitted)
+def create_notifications_for_application_submitted(
+        sender, application: PermitApplication, **kwargs):
+    # Notifications for admins
+    message = f"""
+    A permit application for type {application.get_permit_type_display()} with
+    number {application.no} has been submitted by {application.client.name}.
+    """
+    admins = get_admins_who_can_receive_emails()
+    for admin in admins:
+        Notification.objects.create(
+            user=admin,
+            message=message,
+            url=application.admin_url)
+
+
 @receiver(application_unsubmitted)
 def receive_application_unsubmitted(sender, application: PermitApplication, **kwargs):
     message = f'Permit application {application.no} is unsubmitted.'
@@ -77,6 +95,21 @@ def receive_application_unsubmitted(sender, application: PermitApplication, **kw
             action_flag=CHANGE,
             change_message=_(message)
         )
+
+
+@receiver(application_unsubmitted)
+def create_notifications_for_application_unsubmitted(
+        sender, application: PermitApplication, **kwargs):
+    # Notifications for admins
+    message = f"""
+    A permit application for type {application.get_permit_type_display()} with
+    number {application.no} has been unsubmitted by {application.client.name}.
+    """
+    admins = get_admins_who_can_receive_emails()
+    for admin in admins:
+        Notification.objects.create(
+            user=admin,
+            message=message)
 
 
 @receiver(application_accepted)
@@ -100,8 +133,20 @@ def receive_application_accepted(sender, application: PermitApplication, **kwarg
     message = f'''
     Your permit application <a href="{url}">{application.no}</a> has been accepted.
     '''
+
+
+@receiver(application_accepted)
+def create_notifications_application_accepted(
+        sender, application: PermitApplication, **kwargs):
+    # Notifications for client
+    message = f"""
+    Your permit application for type {application.get_permit_type_display()}
+    with number {application.no} has been accepted.
+    """
     Notification.objects.create(
-        user=application.client, message=message)
+        user=application.client,
+        message=message,
+        url=application.client_url)
 
 
 @receiver(application_returned)
@@ -121,12 +166,20 @@ def receive_application_returned(sender, application: PermitApplication, **kwarg
             change_message=_(message)
         )
 
-    url = reverse_lazy('update_application', args=[application.id])
-    message = f'''
-    Your permit application <a href="{url}">{application.no}</a> has been returned.
-    '''
+
+@receiver(application_returned)
+def create_notifications_for_application_returned(
+        sender, application: PermitApplication, **kwargs):
+    # Notifications for client
+    message = f"""
+    Your permit application for type {application.get_permit_type_display()}
+    with number {application.no} was returned. Please check the admin's
+    remarks explaining the needed action.
+    """
     Notification.objects.create(
-        user=application.client, message=message)
+        user=application.client,
+        message=message,
+        url=application.client_url)
 
 
 @receiver(inspection_scheduled)
