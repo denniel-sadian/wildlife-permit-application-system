35e244ddafac17608ea152708cc3c30fd697096c
Denniel <sadiandenniel@gmail.com>
2024-03-13 13:21:32 +0800

    Fix null error in `changeform_view`method

--- users/mixins.py
+++ users/mixins.py
@@ -62,9 +62,10 @@ class AdminMixin:
 
     def changeform_view(self, request, object_id=None, form_url='',
                         extra_context=None):
-        extra_context['show_save'] = False
-        extra_context['show_save_as_new'] = False
-        extra_context['show_save_and_add_another'] = False
+        if extra_context:
+            extra_context['show_save'] = False
+            extra_context['show_save_as_new'] = False
+            extra_context['show_save_and_add_another'] = False
         return super().changeform_view(request, object_id, form_url,
                                        extra_context)
 
